27884826a9aacb3a703f9213cc921d5bdfe15644
alecley <164290076+alecleypereira@users.noreply.github.com>
2025-10-14 01:26:06 -0300

    ver como implementar onclick e operacoes

---
+++ script.js
@@ -0,0 +1,26 @@
+button_excluir = document.getElementById("button-3")
+button_divisao = document.getElementById("button-4")
+button_produto = document.getElementById("button-8")
+button_soma = document.getElementById("button-16")
+button_subtrair = document.getElementById("button-12")
+button_resultado = document.getElementById("button-20")
+button_virgula = document.getElementById("button-19")
+button_mudar_sinal = document.getElementById("button-17")
+
+
+button_n0 = document.getElementById("button-18")
+button_n1 = document.getElementById("button-15")
+button_n2 = document.getElementById("button-14")
+button_n3 = document.getElementById("button-13")
+button_n4 = document.getElementById("button-11")
+button_n5 = document.getElementById("button-10")
+button_n6 = document.getElementById("button-9")
+button_n7 = document.getElementById("button-7")
+button_n8 = document.getElementById("button-6")
+button_n9 = document.getElementById("button-5")
+
+button_n0.addEventListener("click", soma) //dando erro nessa linha
+
+function soma(){
+    alert("valor =" + button_0)
+}
